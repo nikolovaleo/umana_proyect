152ac9abc30ce8cf81a8a278574282b6390de646
rbastor16 <117126202+rbastor16@users.noreply.github.com>
2022-10-31 16:53:59 -0600

    archivo

--- Umana_proyect.py
+++ Umana_proyect.py
@@ -8,9 +8,10 @@ print(data.sheet_names)
 
 
 
+
+
+
 # Para ver el contenido de una de las hojas se utiliza la siguiente sintaxis: X_sheet = data.parse("Nombre de la hoja en excel")
 # Para accesar a una columna de una hoja en especifico: X_sheet["Nombre de la columna en la hoja"]
 # Para accesar a un elemento de una celda en una hoja y columna especifica: X_sheet["Nombre de la columna"][posicion numerada desde 0 hasta n]
 # Para conocer el tamano de una columna en especifico X_sheet["Columna"].size
-
-#comentario
