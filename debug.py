e2cef141f062c410b2fbfecba5cf09a7c611c4c9
nikolovaleo <nikolovaleo@gmail.com>
2022-11-01 09:13:30 -0600

    addins

--- debug.py
+++ debug.py
@@ -1,19 +1,30 @@
-import pandas as pd 
-file = "test.xlsx" 
-data = pd.ExcelFile(file)
-
-names_sheets = data.sheet_names
-
-#print(names_sheets)
+import pandas as pd
 
 
-fieldbus_names = []
-
-for sheet in names_sheets:
-
-    if 'Fieldbus' in sheet and 'OvationFieldbusPort' not in sheet:
-
-        fieldbus_names.append(sheet)
+import os
+#from os import system, name, getcwd
+#import win32com.client as win32
+from win32com.client.gencache import EnsureDispatch
+from pathlib import Path
 
 
-print(fieldbus_names)
+
+excel = EnsureDispatch('Excel.Application')
+excel.Visible = True
+
+
+f_path = (os.getcwd() + '\\data\\4-17HPS_LIT_030A.xlsx')
+
+
+addin_path = r'C:\Data\Excel Addins\OvationUtils\Addin\OvationUtils-F07d.xla'
+excel.Workbooks.Open(addin_path)
+excel.AddIns.Add(addin_path).Installed = True
+
+#excel.RegisterXLL('C:/Data/Excel Addins/OvationUtils/Addin/OvationUtils-F07d.xla')
+#excel.COMAddIns("OvationUtils-F07d.ExcelAddIn").Connect = True
+wb1_raw = excel.Workbooks.Open(f_path)
+
+
+#wb1_raw.RefreshAll()
+#wb1_raw.SaveAs()
+#wb1_raw.Close(True)
