f5101ba0fc71e6c82051bb6252cf51cb72b79c04
rbastor16 <117126202+rbastor16@users.noreply.github.com>
2022-11-01 07:23:40 -0600

    Creacion de hoja

--- Umana_proyect.py
+++ Umana_proyect.py
@@ -1,4 +1,6 @@
 import pandas as pd 
+from xlsxwriter import Workbook
+import openpyxl
 file = "test.xlsx" 
 data = pd.ExcelFile(file)
 fieldbus_device_sheet = data.parse("FieldbusDevice")
@@ -6,23 +8,35 @@ names_sheets = data.sheet_names
 fieldbus_sheets = []
 # Ciclo que toma las hojas que contienen la palabra "Fieldbus" en su nombre y las guarda en la lista vacia llamada Fieldbus_sheets
 for sheet in names_sheets:
-    if ('Fieldbus' in sheet) and ('OvationFieldbusPort' not in sheet) and ('FieldbusDevice' not in sheet):
+    if ('Fieldbus' in sheet) and ('OvationFieldbusPort' not in sheet) and ('FieldbusDevice' not in sheet) and ("FieldbusVCR" not in sheet):
         fieldbus_sheets.append(sheet)
 
 
-
-
-
+print(fieldbus_sheets)
 
 first_device =  "17HPS_LIT_030A" #fieldbus_device_sheet["ObjectName"][0]
-#for data_sheet in fieldbus_sheets:
+
+
 
 new = data.parse("FieldbusDevice")["ObjectName"].isin([first_device])
 data1 = data.parse("FieldbusDevice")
 print(data1[new])
 
+
 archivo = data1[new]
-archivo.to_excel("output.xlsx", sheet_name="FieldbusDevice")
+archivo.to_excel(f'{first_device}.xlsx', sheet_name="FieldbusDevice")
+
+
+for f_sheet in fieldbus_sheets:
+    new2 = data.parse(f_sheet)["ParName_11"].isin([first_device])
+    data2 = data.parse(f_sheet)
+    print(data2[new2])
+    archivo2 = data1[new]
+    write = pd.ExcelWriter(f'{first_device}.xlsx', engine='openpyxl' ,mode= "a")
+    archivo2.to_excel(write, sheet_name = f_sheet, index = False)
+    #archivo2.to_excel(f'{first_device}.xlsx', sheet_name = f_sheet, index = False)
+
+
 
 #column_names = []
 #for sheet in fieldbus_names:
